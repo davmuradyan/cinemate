12f70774cf831117ad63740be4af208962e0a351
David Muradyan <i22.muradyan.david@etud.ufar.am>
2024-12-14 14:44:20 +0400

    c

---
+++ src/Intro/Paragraph.js
@@ -0,0 +1,15 @@
+import './Paragraph.css'
+
+function Paragraph() {
+    return (
+        <div className='ParagraphDiv'>
+            <p className='Paragraph'>
+            Discover your next favorite film with personalized
+            recommendations. Rate movies you love, and let 
+            Cinemate curate a list tailored just for you!
+            </p>
+        </div>
+    )
+}
+
+export default Paragraph
--- src/Pages/Intro.js
+++ src/Pages/Intro.js
@@ -1,10 +1,13 @@
 import Navbar from "../Intro/Navbar";
 import './Css/Intro.css'
+import './../Intro/Paragraph'
+import Paragraph from "./../Intro/Paragraph";
 
 function Intro() {
     return (
       <div className="Intro">
         <Navbar />
+        <Paragraph />
       </div>
     );
 }
